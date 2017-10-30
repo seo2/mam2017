c4865d2aa32d993182a95568b0399e0f3b29d258
Nicolas cajas <nico@seo2.cl>
2017-10-30 18:34:41 -0300

    menú

--- include-top.php
+++ include-top.php
@@ -40,13 +40,13 @@ if(ICL_LANGUAGE_CODE=='en'){
 	$btn2 = "Veja mais";
 }else{ 	
     $menu2 = "distrito de lujo";
-    $menu3 = "boulevard";
-    $menu4 = "piso diseño";
+    $menu3 = "patio de comidas";
+    $menu4 = "Restaurantes";
     $menu5 = "entretención";
     $menu6 = "novedades";
     $menu7 = "tiendas";
-	$menu8 = "arauco tag";
-    $menu9 = "revista detalle";
+	$menu8 = "galería";
+    $menu9 = "destacado de la semana";
     $menu10 = "servicio al cliente";
     $menu11 = "travellers";
     
@@ -136,12 +136,13 @@ if(ICL_LANGUAGE_CODE=='en'){
                                      <li><a id="btn_novedades" 		href="<?php bloginfo('url'); ?>/novidades#novedades"><?php echo $menu6; ?></a></li>
                                      <li><a id="btn_tiendas" 		href="<?php bloginfo('url'); ?>/tiendas#tiendas"><?php echo $menu7; ?></a></li>
                                     <?php }else{ 	?>
-                                     <li><a id="btn_distrito" 		href="<?php bloginfo('url'); ?>/distrito-de-lujo"><?php echo $menu2; ?></a></li>
+                                     <!-- <li><a id="btn_distrito" 		href="<?php bloginfo('url'); ?>/distrito-de-lujo"><?php echo $menu2; ?></a></li> -->
+                                      <li><a id="btn_tiendas"     href="<?php bloginfo('url'); ?>/tiendas#tiendas"><?php echo $menu7; ?></a></li>
                                      <li><a id="btn_boulevard" 		href="<?php bloginfo('url'); ?>/boulevard#boulevard"><?php echo $menu3; ?></a></li>
                                      <li><a id="btn_piso" 			href="<?php bloginfo('url'); ?>/piso-diseno"><?php echo $menu4; ?></a></li>
                                      <li><a id="btn_entretencion" 	href="<?php bloginfo('url'); ?>/novedades#entretencion"><?php echo $menu5; ?></a></li>
                                      <li><a id="btn_novedades" 		href="<?php bloginfo('url'); ?>/novedades#novedades"><?php echo $menu6; ?></a></li>
-                                     <li><a id="btn_tiendas" 		href="<?php bloginfo('url'); ?>/tiendas#tiendas"><?php echo $menu7; ?></a></li>
+                                    
                                     <?php } ?>
                                 </ul>
                             </div>
@@ -153,10 +154,11 @@ if(ICL_LANGUAGE_CODE=='en'){
                                        <li class="hidden-xs hidden-sm"><a class="invisible" href="javascript:void(0);">&nbsp</a></li>
                                        <li class="hidden-xs hidden-sm"><a class="invisible" href="javascript:void(0);">&nbsp</a></li>
                                        <li class="hidden-xs hidden-sm"><a class="invisible" href="javascript:void(0);">&nbsp</a></li>
-                                       <li><a href="<?php bloginfo('url'); ?>/arauco-tag"><?php echo $menu8; ?></a></li>
+                                       <li><a href="<?php bloginfo('url'); ?>/novedades-galeria/#galerias"><?php echo $menu8; ?></a></li>
 <!--                                        <li><a href="http://www.revistadetalle.cl/" target="_blank"><?php echo $menu9; ?></a></li> -->
+                                      <li><a href="<?php bloginfo('url'); ?>/arauco-tag"><?php echo $menu9; ?></a></li>
                                        <li><a href="<?php bloginfo('url'); ?>/servicio-al-cliente"><?php echo $menu10; ?></a></li>
-                                       <li><a href="<?php bloginfo('url'); ?>/travellers"><?php echo $menu11; ?></a></li>
+                                       <!-- <li><a href="<?php bloginfo('url'); ?>/travellers"><?php echo $menu11; ?></a></li> -->
                                   </ul>
                             </div>
 
