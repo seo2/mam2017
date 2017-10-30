2df82c9600fde06b295827816b58a6ddaec678b9
fchristiansen <felipe@seo2.cl>
2017-10-30 18:07:07 -0300

    ajuste novedades WP

--- novedades.php
+++ novedades.php
@@ -9,7 +9,7 @@ Template name: Novedades
 <?php include('include-top.php') ?>
 <?php include('include-slider-home.php') ?>
 <?php include('include-search-home.php') ?>
-<?php 
+<?php
 // TRADUCCIONES
 if(ICL_LANGUAGE_CODE=='en'){
 	$tit1 = "NEWS OF THE MONTH";
@@ -17,19 +17,19 @@ if(ICL_LANGUAGE_CODE=='en'){
 	$tit3 = "HIGHLIGHTS";
 	$btn1 = "More information";
 	$btn2 = "View more restaurants";
-}elseif(ICL_LANGUAGE_CODE=='pt-br'){ 
+}elseif(ICL_LANGUAGE_CODE=='pt-br'){
 	$tit1 = "Novidades DO MÊS";
 	$tit2 = "Diversão";
 	$tit3 = "DESTACADOS";
 	$btn1 = "mais informação";
 	$btn2 = "Veja mais restaurantes";
-}else{ 
+}else{
 	$tit1 = "NOVEDADES DEL MES";
 	$tit2 = "ENTRETENCIÓN";
 	$tit3 = "destacados";
 	$btn1 = "más información";
 	$btn2 = "ver más restaurantes";
-} ?> 
+} ?>
     <section class="main_content">
 	    <div class="titulo_seccion ">
 	        <div class="container">
@@ -50,16 +50,17 @@ if(ICL_LANGUAGE_CODE=='en'){
               <div id="novedades" class="anchor_seccion"></div>
                       <div class="grid_tiendas center-block">
                           <div class="container">
-<?php	                                         
-    $args = array(
-		'post_type' => array('novedades_top')
-    );
-	$the_query = new WP_Query ($args);
-	
-    if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();
-?> 
-                        	<div class="row">
-                            	<div class="col-sm-4 col-md-3"> 
+                              <?php
+                                  $args = array(
+                              		'post_type' => array('novedades_top')
+                                  );
+                              	$the_query = new WP_Query ($args);
+
+                                  if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();
+                              ?>
+
+                          <div class="row">
+                            	<div class="col-sm-4 col-md-3">
                                 	<div class="item_tienda">
                                         <div class="tienda">
                                         	<a class="" href="<?php echo get('caluga_cine_link'); ?>" target="_blank">
@@ -71,15 +72,16 @@ if(ICL_LANGUAGE_CODE=='en'){
                                               	</div> <!-- logo_tienda -->
                                                 <div class="desc_destacado visible-xs">
                                                     <h3>¡Descubre los mejores estrenos!</h3>
-                                                    <p>Si eres amante del cine, entra aquí y conoce las últimas novedades en la cartelera de Hoyts Parque Arauco.</p>
-                                                    <a href="http://www.cinehoyts.cl/cartelera/santiago-oriente/parque-arauco" target="_blank">www.cinehoyts.cl</a>
+                                                    <p>Si eres amante del cine, entra aquí y conoce las últimas
+                                                    novedades en la cartelera de Hoyts Parque Arauco.</p>
+                                                    <a href="http://www.cinehoyts.cl/cartelera/santiago-poniente-y-norte/maipu" target="_blank">www.cinehoyts.cl</a>
                                                 </div>
                                             </div> <!-- box logo tienda -->
                                         </div> <!--  tienda -->
                                   	</div> <!-- item tienda -->
 								</div> <!-- col sm 4 -->
-								
-								<div class=" col-sm-8 col-md-9"> 
+
+								<div class=" col-sm-8 col-md-9">
 									<div class="box_horizontal box_novedades">
 										<div class="box_slider">
 											<div class="item disable-owl-swipe">
@@ -89,63 +91,61 @@ if(ICL_LANGUAGE_CODE=='en'){
 														<div class="">
 															<a class="btn btn-default btn_blanco" href="<?php echo get('banner_grande_link'); ?>" role="button"><?php echo $btn1; ?></a>
 														</div>
-													</div> 
+													</div>
                         						</div><!-- image dynamic -->
 											</div><!-- item -->
 										</div> <!-- box slider -->
 									</div> <!-- box horizontal -->
 								</div> <!-- col sm 12 -->
 							</div><!-- fin row -->
-						    <div class="row">
-						        <section class="entretencion clearfix">				    
-							   	<?php
-								   	$e = 0;
-									$sliders = get_order_group('destacados_imagen');
-									foreach($sliders as $slider){  
-										$e++;
-										if($e<=4){ 
-								?> 
-								    <div class="col-xs-6 col-sm-3 col-md-3">
-								        <div class="box_servicio" >
-									        <a href="<?php echo get('destacados_link',$slider); ?>">
-									            <img class="img-responsive" src="<?php echo get('destacados_imagen',$slider); ?>" alt="">
-									        </a>
-								        </div>
-								        <div class="box_more_info">
-								          <a  href="<?php echo get('destacados_link',$slider); ?>"><?php echo get('destacados_texto',$slider); ?></a>
-								        </div>
-								    </div>
-								<?php 
-										}
-									} ?>   
-							    </section>
-							</div>
-							<?php endwhile; else: ?>
-							<?php endif; ?>             
-                        </div> <!-- container grilla novedades -->
-         
-                      </div><!-- grid tiendas -->
-              
-              </section> <!-- end tiendas -->
+						   <!--  <div class="row">
+
+                        <section class="entretencion clearfix">
+          							   	<?php
+          								   	$e = 0;
+          									$sliders = get_order_group('destacados_imagen');
+          									foreach($sliders as $slider){
+          										$e++;
+          										if($e<=4){
+          								?>
+      								    <div class="col-xs-6 col-sm-3 col-md-3">
+      								        <div class="box_servicio" >
+      									        <a href="<?php echo get('destacados_link',$slider); ?>">
+      									            <img class="img-responsive" src="<?php echo get('destacados_imagen',$slider); ?>" alt="">
+      									        </a>
+      								        </div>
+      								        <div class="box_more_info">
+      								          <a  href="<?php echo get('destacados_link',$slider); ?>"><?php echo get('destacados_texto',$slider); ?></a>
+      								        </div>
+      								    </div>
+      								<?php
+      										}
+      									} ?>
+      							    </section>
+      							</div>
+      							<?php endwhile; else: ?>
+      							<?php endif; ?> -->
+                </div> <!-- container grilla novedades -->
+            </div><!-- grid tiendas -->
+        </section> <!-- end tiendas -->
       </div>
 
     <div class="row">
         <section class="entretencion clearfix">
         	<div id="entretencion" class="anchor_entretencion"></div>
-			<h4 class="divider"><?php echo $tit2; ?></h4>    
-<?php 
+			<h4 class="divider"><?php echo $tit2; ?></h4>
+<?php
     $detect = new Mobile_Detect();
-    	 
-	if ($detect->isMobile()) { 
-	   include('include-entretencion-mobile.php'); 
-	}else{ 	 
+
+	if ($detect->isMobile()) {
+	   include('include-entretencion-mobile.php');
+	}else{
 	  	include('include-entretencion-desktop.php');
 	}
 ?>
-   
         </section>
     </div><!-- row -->
-      
+
 <div id="pages" style="display:none;" data-pagina="boulevard"></div>
 <?php include('footer.php') ?>
 
